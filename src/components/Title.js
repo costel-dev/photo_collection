1b692ddc14fd2c736f5824ddaa158e3c48d50898
costel-dev <costelcuconoiu@gmail.com>
2020-11-30 16:47:42 +0100

    initial commit

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>Photo_Collection</h1>
+      <h2>Your Pictures</h2>
+      <p>Photo Collection Project with React and Firebase</p>
+    </div>
+  )
+}
+
+export default Title;
